9277e537b5f9764566e26cce4e367cdd22c074a9
Serein <732687813@qq.com>
2022-03-30 20:09:33 +0800

    Update Twitter Crawler

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from selenium.webdriver import ChromeOptions
 from selenium import webdriver
 import os
 
+
 def Chrome_Config(Chrome_path):
     options = ChromeOptions()
     options.add_experimental_option('excludeSwitches', ['enable-automation'])
@@ -13,23 +14,24 @@ def Chrome_Config(Chrome_path):
 
     driver = webdriver.Chrome(Chrome_path, options=options)
     return driver
+
+
 if __name__ == '__main__':
 
     Chrome_path = "F:\Code//2022\chromedriver_win32\chromedriver.exe"  # Enter your chrome driver path here
-    driver =Chrome_Config(Chrome_path) #In this function, you can config your chrome driver
-
+    driver = Chrome_Config(Chrome_path)  # In this function, you can config your chrome driver
     print('------------------------------------------------------------------------')
-    #--------------------------------------------------------------------------------
-    #parameters
+    # --------------------------------------------------------------------------------
+    # parameters
     Keyword_Path = 'keyword.csv'
-    Stop_num=10000 #this is the number of the items you want to crawl
-    kw_start_point=0 #this parameter decides the start keyword of the crawler.its default value is 0
-    save_path = 'data'#this is the path where you want to save the crawled data
-    start_date= '2021-01-01' #this parameter decides the start date of the crawler.its default value is 2021-01-01
-    end_date= '2020-01-01' #this parameter decides the end date of the crawler.its default value is 2020-01-01
-    #----------------------------------------------------------------------------------
+    Stop_num = 10000  # this is the number of the items you want to crawl
+    kw_start_point = 0  # this parameter decides the start keyword of the crawler.its default value is 0
+    save_path = 'data'  # this is the path where you want to save the crawled data
+    start_date = '2021-01-01'  # this parameter decides the start date of the crawler.its default value is 2021-01-01
+    end_date = '2020-01-01'  # this parameter decides the end date of the crawler.its default value is 2020-01-01
+    # ----------------------------------------------------------------------------------
     if not os.path.exists(save_path):
         os.mkdir(save_path)
-    Twitter_Crawler(driver,Keyword_Path,Stop_num,kw_start_point,save_path,start_date,end_date)
+    Twitter_Crawler(driver, Keyword_Path, Stop_num, kw_start_point, save_path, start_date, end_date)
     print('------------------------------------------------------------------------')
     driver.close()
